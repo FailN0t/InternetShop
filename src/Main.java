eaa189ed7c15b154e636da5ab2472f44daf4e2e4
Igor Putyajkin <putyajkin99@gmail.com>
2023-09-08 05:15:57 +0300

    lern 5.6

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        Basket vasyaBasKet = new Basket();
-        vasyaBasKet.add("Молоко", 80);
-        vasyaBasKet.add("Хлеб", 40);
-
-        Basket petyaBasket = new Basket(500);
-        petyaBasket.add("Лопата", 280);
-        petyaBasket.add("Бочка", 2900);
-
-        vasyaBasKet.print("Корзина Васи: ");
-        petyaBasket.print("Корзина Пети: ");
-
-        Basket mashaBasket = new Basket("\nСтол - " + 5000, 5000);
-        mashaBasket.print("Корзина Маши: ");
-
+        Product product = new Product("Молоко", 56);
+        Order order = new Order();
+        order.addProduct(product);
+        System.out.println(product);
     }
 }
--- src/Order.java
+++ src/Order.java
@@ -1,12 +1,9 @@
 public class Order {
-    private int count = 0;
-
-    public void addProducts(int productsCount){
-        count += productsCount;
-    }
-
-    public int getProductsCount(){
-        return count;
+    public void addProduct(Product product){
+        //adds product
+        if(product.getPrice() > 50){
+            product.setPrice(product.getPrice()-25);
+        }
     }
 
 }
